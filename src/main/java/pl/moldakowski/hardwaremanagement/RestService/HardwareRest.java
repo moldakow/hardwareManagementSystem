c3b0e84dbc336006413f79f10e19ffa6087d821a
moldakow <moldakow@o2.pl>
2021-01-19 19:29:29 +0100

    Comm

--- src/main/java/pl/moldakowski/hardwaremanagement/RestService/HardwareRest.java
+++ src/main/java/pl/moldakowski/hardwaremanagement/RestService/HardwareRest.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.moldakowski.hardwaremanagement.entities.Hardware;
-import pl.moldakowski.hardwaremanagement.repositories.HardwareRepository;
 import pl.moldakowski.hardwaremanagement.services.HardwareService;
 
 import java.util.List;
